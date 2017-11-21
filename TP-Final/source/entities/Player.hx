af6392d34d1b79047083bf87b227969ceaa693ad
Nicolas Piccitto <nicolaspiccitto@gmail.com>
2017-11-20 23:16:14 -0300

    maquina de estado

--- TP-Final/source/entities/Player.hx
+++ TP-Final/source/entities/Player.hx
@@ -2,34 +2,157 @@ package entities;
 import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.effects.FlxFlicker;
+import flixel.text.FlxText;
 
 /**
  * ...
  * @author Nicolas Piccitto
  */
+enum States
+{
+	IDLE;
+	RUN;
+	JUMP;
+	ATTACK;
+
+}
 class Player extends FlxSprite
 {
+	public var currentState(get, null):States;
+	private var lives:Int;
+	private var timer:Float;
+	private var youdie:FlxText;
+	
 	public function new(?X:Float = 0, ?Y:Float = 0, ?SimpleGraphic:FlxGraphicAsset)
 	{
 		super(X, Y, SimpleGraphic);
 		makeGraphic(5, 5, 0xffffffff);
+		currentState = States.IDLE;
+		lives = 1;
 		
 	}
 	
 	override public function update(elapsed:Float)
 	{
+		stateMachine();
 		super.update(elapsed);
-		movement();
+		
 	}
-	private function movement()
+	private function stateMachine():Void
 	{
-		if (FlxG.keys.pressed.RIGHT)
-			velocity.x += 5;
-		if (FlxG.keys.pressed.LEFT)
-			velocity.x -= 5;
-		if (FlxG.keys.pressed.UP)
-			velocity.y -= 5;
-		if (FlxG.keys.pressed.DOWN)
-			velocity.y += 5;
+
+		switch (currentState)
+		{
+			case States.IDLE:
+				attack();
+				horizontalMovement();
+				jump();
+				//animation.play("idle");
+				if (velocity.y != 0)
+					currentState = States.JUMP;
+				else if (velocity.x != 0)
+					currentState = States.RUN;
+
+			case States.RUN:
+				attack();
+				horizontalMovement();
+				jump();
+				//animation.play("run");
+				if (velocity.y != 0)
+					currentState = States.JUMP;
+				else if (velocity.x == 0)
+					currentState = States.IDLE;
+
+			case States.JUMP:
+				attack();
+				//animation.play("jump");
+
+				if (velocity.y == 0)
+				{
+					if (velocity.x == 0)
+						currentState = States.IDLE;
+					else
+						currentState = States.RUN;
+				}
+
+			case States.ATTACK:
+				//animation.play("attack");
+				this.velocity.x = 0;
+				attack();
+				
+
+		}
 	}
+	private function horizontalMovement():Void //esta haciendo conflicto con el objeto deslizante
+	{
+		velocity.x = 0; //esto hace que no se mueva al parecer
+
+		if (FlxG.keys.pressed.RIGHT)
+		{
+			velocity.x += 100;
+			facing = FlxObject.RIGHT;
+		}
+		if (FlxG.keys.pressed.LEFT)
+		{
+			velocity.x -= 100;
+			facing = FlxObject.LEFT;
+		}
+	}
+
+	private function jump():Void
+	{
+		if (FlxG.keys.justPressed.UP && isTouching(FlxObject.FLOOR))
+		{
+			velocity.y = -400;
+
+		}
+	}
+
+	function get_currentState():States
+	{
+		return currentState;
+	}
+
+	public function attack():Void
+	{
+
+		
+
+	}
+
+	public function getDamage(damage:Int)
+	{
+		if (lives<=0)
+		{	
+			youdie = new FlxText (this.x-30, this.y-5, 0, "Press R to restart, Esc to quit", 7);
+			FlxG.state.add(youdie);
+			this.kill();
+			
+		
+		}
+		if (!FlxFlicker.isFlickering(this))
+		{
+			lives -= damage;
+			if (lives >= 0)
+				FlxFlicker.flicker(this, 3, 0.08, true, true);
+
+		}
+	}
+
+	public function die():Void
+	{
+		lives--;
+		if (lives<=0)
+		{
+			youdie = new FlxText (this.x-30, this.y-8, 0, "Press R to restart, Esc to quit", 7);
+			FlxG.state.add(youdie);
+			this.kill();
+			
+			
+		}
+	}
+
+	
 }
