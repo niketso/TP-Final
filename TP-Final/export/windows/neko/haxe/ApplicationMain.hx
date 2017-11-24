c8b734551a6896bd7e5083e57a59c73f2f7983fb
Unknown <nicolaspiccitto@gmail.com>
2017-11-24 15:11:18 -0300

    Agrego enemigos

--- TP-Final/export/windows/neko/haxe/ApplicationMain.hx
+++ TP-Final/export/windows/neko/haxe/ApplicationMain.hx
@@ -515,7 +515,7 @@ class ApplicationMain {
 		
 		config = {
 			
-			build: "16",
+			build: "18",
 			company: "Nicolas Piccitto",
 			file: "TP-Final",
 			fps: 60,
---
+++ TP-Final/source/entities/Enemy2.hx
@@ -0,0 +1,18 @@
+package entities;
+
+import flixel.system.FlxAssets.FlxGraphicAsset;
+
+/**
+ * ...
+ * @author ...
+ */
+class Enemy2 extends Enemy 
+{
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
+	{
+		super(X, Y, SimpleGraphic);
+		
+	}
+	
+}
